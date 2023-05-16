f99c4a180d8d4632ab2061f6bab78f354256f3d8
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-05-16 19:49:39 +0300

    add fix

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,32 @@
+// імпорт бібліотек
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+// стилізація нашого компонента
+import { Li, Thumb, Img } from './ImageGalleryItem.styled';
+
+export default class ImageGalleryItem extends Component {
+  onClickShowPhoto = () => {
+    const { setImageLink, togleModal, image } = this.props;
+    setImageLink(image.largeImageURL);
+    togleModal();
+  };
+
+  render() {
+    const { image } = this.props;
+    return (
+      <Li>
+        <Thumb onClick={this.onClickShowPhoto}>
+          <Img src={image.webformatURL} alt={image.tags} loading="lazy" />
+        </Thumb>
+      </Li>
+    );
+  }
+}
+
+// перевірка PropTypes
+ImageGalleryItem.propTypes = {
+  image: PropTypes.object.isRequired,
+  setImageLink: PropTypes.func.isRequired,
+  togleModal: PropTypes.func.isRequired,
+};
